68df692c51efc6c25e66393672e8b1a020310cee
Vicky Yin <v-vyin@microsoft.com>
2020-10-07 10:14:16 +0800

    first commit

---
+++ Crash_AE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using SqlServerHelper.Core;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Crash_AE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string
+            FileInfo excel = new FileInfo();
+        }
+    }
+}
